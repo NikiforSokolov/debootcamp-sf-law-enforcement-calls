fc088ca141c10eb54fee0fe36f8f3d64ccc012fe
danihello <danihello@gmail.com>
2024-07-11 12:25:01 +0300

    changed_some_comments_on_orchestrator

--- orchestraitor.sh
+++ orchestraitor.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# go the extract load directory and run airbyte
+#go the extract load directory
 cd extract_load && 
+#run airbyte
 python -m pipelines.airbyte_extract_load &&
-# go to the transform directory and run dbt
+#go to the transform directory 
 cd .. &&
 cd transform/warehouse &&
+#run dbt
 dbt deps &&
 dbt build --target prod
