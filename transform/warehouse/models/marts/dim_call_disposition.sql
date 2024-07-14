7750a8965b69245f14b8e0c96d456f70ed607f09
NikiforSokolov <nsokolov@purestorage.com>
2024-07-14 17:40:44 +0200

    formatting mart models

--- transform/warehouse/models/marts/dim_call_disposition.sql
+++ transform/warehouse/models/marts/dim_call_disposition.sql
@@ -1,5 +1,5 @@
 
 select 
-{{ dbt_utils.generate_surrogate_key(['call_disposition_lkp.call_disposition_id']) }} as call_disposition_key
-,*
+    {{ dbt_utils.generate_surrogate_key(['call_disposition_lkp.call_disposition_id']) }} as call_disposition_key,
+    *
 from {{ ref('call_disposition_lkp') }} as call_disposition_lkp
--- transform/warehouse/models/marts/dim_call_types.sql
+++ transform/warehouse/models/marts/dim_call_types.sql
@@ -1,7 +1,7 @@
 select
-{{ dbt_utils.generate_surrogate_key(['call_types_code']) }} as call_types_key,
-call_types_priority,
-call_types_code,
-call_types_description,
-call_types_data_as_of
-from {{ref('call_types')}}
+    {{ dbt_utils.generate_surrogate_key(['call_types_code']) }} as call_types_key,
+    call_types_priority,
+    call_types_code,
+    call_types_description,
+    call_types_data_as_of
+from {{ ref('call_types') }}
